ac4252c0d155f8780544b00f8c518041471223c0
sanket pathak <sanketpathak64@gmail.com>
2020-03-19 21:53:06 +0530

    added navbar

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import { Container} from 'semantic-ui-react';
+
+export default props => {
+    return(
+
+        <div>
+            <Container>
+            <Header />
+            {props.children}
+            </Container>
+            
+        </div>
+    );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,44 @@
 //npm run dev
 import React,{Component} from 'react';
 import factory from '../ethereum/factory';
-
-
+import {Card,Button} from 'semantic-ui-react';
+import Layout from '../components/Layout';
 
 class CampaignIndex extends Component{
     static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
         return { campaigns };
     }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+           return {
+            header: address,
+            description: <a> View Campaign</a>,
+            fluid: true
+           } ;
+        });
+
+        return <Card.Group items={items} />;
+    }
+
     render() {
-        return <div> {this.props.campaigns[0]} </div>;
+       
+
+        return (
+        <Layout><div>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" /> 
+        
+        <h3>Open Campaign</h3>
+        {this.renderCampaigns()}
+        <Button 
+            content="Create Campaign"
+            icon="add circle" //add also
+            primary // primary={true}
+        />
+
+        </div>
+        </Layout>);
     }
 }
 
